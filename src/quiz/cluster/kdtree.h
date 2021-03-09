909c67a173f85c7e2ca526f6d70f89cef99762b2
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-09 22:58:06 +0100

    feat: implemented point search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -41,10 +41,37 @@ struct KdTree {
     insertHelper(&root, 0, point, id);
   }
 
+  void searchHelper(std::vector<float> target, Node *node, int depth,
+                    float distanceTol, std::vector<int> &ids) {
+
+    if (node != NULL) {
+      if ((node->point[0] >= (target[0] - distanceTol) &&
+           node->point[0] <= (target[0] + distanceTol)) &&
+          (node->point[1] >= (target[1] - distanceTol) &&
+           node->point[1] <= (target[1] + distanceTol))) {
+
+        float distance =
+            sqrt((node->point[0] - target[0]) * (node->point[0] - target[0]) +
+                 (node->point[1] - target[1]) * (node->point[1] - target[1]));
+
+        if (distance <= distanceTol)
+          ids.push_back(node->id);
+
+        if ((target[depth % 2] - distanceTol) < node->point[depth % 2])
+          searchHelper(target, node->left, depth + 1, distanceTol, ids);
+        if ((target[depth % 2] + distanceTol) > node->point[depth % 2])
+          searchHelper(target, node->right, depth + 1, distanceTol, ids);
+      }
+    }
+  }
+
+
   // return a list of point ids in the tree that are within distance of target
   std::vector<int> search(std::vector<float> target, float distanceTol) {
 
     std::vector<int> ids;
+	searchHelper(target, root, 0, distanceTol, ids);
+    
 	return ids;
   }
 

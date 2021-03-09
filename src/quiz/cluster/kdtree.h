87f7499aea2b8287e9d78c5e38bdaa62ecd365c7
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-09 21:00:19 +0100

    feat: inserting points in tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -5,43 +5,47 @@
 
 
 // Structure to represent node of kd tree
-struct Node
-{
-	std::vector<float> point;
-	int id;
-	Node* left;
-	Node* right;
+struct Node {
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+  std::vector<float> point;
+  int id;
+  Node* left;
+  Node* right;
+
+  Node(std::vector<float> arr, int setId)
+      : point(arr), id(setId), left(NULL), right(NULL) {}
 };
 
-struct KdTree
-{
-	Node* root;
+struct KdTree {
+  
+  Node *root;
 
-	KdTree()
-	: root(NULL)
-	{}
+  KdTree() : root(NULL) {}
 
-	void insert(std::vector<float> point, int id)
-	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+  void insertHelper(Node **node, uint depth, std::vector<float> point, int id) {
 
-	}
+    if (*node == NULL)
+      *node = new Node(point, id);
+    else {
+      uint cd = depth % 2;
 
-	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
-		std::vector<int> ids;
-		return ids;
-	}
-	
+      if (point[cd] < ((*node)->point[cd]))
+        insertHelper(&((*node)->left), depth + 1, point, id);
+      else
+        insertHelper(&((*node)->right), depth + 1, point, id);
+    }
+  }
+
+  void insert(std::vector<float> point, int id){
+
+    insertHelper(&root, 0, point, id);
+  }
+
+  // return a list of point ids in the tree that are within distance of target
+  std::vector<int> search(std::vector<float> target, float distanceTol) {
+
+    std::vector<int> ids;
+	return ids;
+  }
 
 };
-
-
-
-

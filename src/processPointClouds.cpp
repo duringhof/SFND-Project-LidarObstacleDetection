fbc611a50f0d525ae0e3e43f0ef583da6e120773
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-10 22:17:32 +0100

    fix: corrected roof point filtering

--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -48,7 +48,7 @@ typename pcl::PointCloud<PointT>::Ptr ProcessPointClouds<PointT>::FilterCloud(
 
   pcl::CropBox<PointT> roof(true);
   roof.setMin(Eigen::Vector4f(-1.5, -1.7, -1, 1));
-  roof.setMax(Eigen::Vector4f(2.6, 1.7, -4, 1));
+  roof.setMax(Eigen::Vector4f(2.6, 1.7, 4, 1));
   roof.setInputCloud(cloudRegion);
   roof.filter(indices);
 

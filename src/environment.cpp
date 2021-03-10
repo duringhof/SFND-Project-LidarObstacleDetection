24d87dc7c8336d1b34718a7e1376776df1c3892f
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-10 22:53:14 +0100

    feat: implemented obstacle detection on real PCD

--- src/environment.cpp
+++ src/environment.cpp
@@ -89,8 +89,30 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr &viewer) {
   pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessorI->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
   //renderPointCloud(viewer, inputCloud, "inputCloud");
 
-  pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessorI->FilterCloud(inputCloud, 0.4 , Eigen::Vector4f (-15, -6.5, -4, 1), Eigen::Vector4f (40, 6.5, 10, 1));
-  renderPointCloud(viewer,filterCloud,"filterCloud");
+  pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessorI->FilterCloud(inputCloud, 0.2 , Eigen::Vector4f (-15, -6.5, -2, 1), Eigen::Vector4f (40, 6.5, 10, 1));
+  //renderPointCloud(viewer, filterCloud, "filterCloud");
+
+  std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->SegmentPlane(filterCloud, 100, 0.2);
+  //renderPointCloud(viewer,segmentCloud.first,"obstCloud",Color(1,0,0));
+  renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
+
+  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters =
+        pointProcessorI->Clustering(segmentCloud.first, 0.5, 30, 500);
+
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1, 0, 0), Color(0, 1, 1),
+                                 Color(0, 0, 1)};
+
+    for (pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters) {
+
+      std::cout << "cluster size ";
+      pointProcessorI->numPoints(cluster);
+      renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId),
+                       colors[clusterId]);
+      Box box = pointProcessorI->BoundingBox(cluster);
+      renderBox(viewer,box,clusterId);
+      ++clusterId;
+    }
 }
 
 //setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}

c9df84927553b094eff1aa4534798c5eb61bb577
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-03-10 10:20:50 +0100

    feat: drawing boundingboxes for found clusters

--- src/environment.cpp
+++ src/environment.cpp
@@ -71,6 +71,8 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
       pointProcessor->numPoints(cluster);
       renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId),
                        colors[clusterId]);
+      Box box = pointProcessor->BoundingBox(cluster);
+      renderBox(viewer,box,clusterId);
       ++clusterId;
     }
 }
